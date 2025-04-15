6cc715291c4ecbf12ac4f4c45f71419b94c22856
HUYQUANGTRAN2 <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-04-15 02:37:31 +0000

    2nd trial

--- app.rb
+++ app.rb
@@ -2,9 +2,48 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb (:main_page)
 end
 
+get ("/rock") do
+  second_hand_choices=["rock","paper","scissors"]
+  second_hand = second_hand_choices.sample
+  if second_hand == "rock"
+    pp outcome = "We tied!"
+  elsif second_hand == "paper"
+    pp outcome = "We lost!"
+  else 
+    pp outcome = "We won!"
+  end 
+  @text_to_appear = ["They played #{second_hand}!","#{outcome}"]
+   erb(:Rock)
+end
+
+get ("/paper") do
+  second_hand_choices=["rock","paper","scissors"]
+  second_hand = second_hand_choices.sample
+  if second_hand == "rock"
+    pp outcome = "We won!"
+  elsif second_hand == "paper"
+    pp outcome = "We tied!"
+  else 
+    pp outcome = "We lost!"
+  end 
+  @text_to_appear = ["They played #{second_hand}!","#{outcome}"]
+  erb(:Paper)
+end
+
+get ("/scissors") do
+  second_hand_choices=["rock","paper","scissors"]
+  second_hand = second_hand_choices.sample
+  if second_hand == "rock"
+    pp outcome = "We lost!"
+  elsif second_hand == "paper"
+    pp outcome = "We won!"
+  else 
+    pp outcome = "We tied!"
+  end 
+  @text_to_appear = ["They played #{second_hand}!","#{outcome}"]
+
+  erb(:Scissors)
+end 
